2ff67c4a9731be4f8890a0cd4fb0dddffcdff48f
eapepe0 <eapepe0@gmail.com>
2022-10-24 03:01:57 -0300

    2do ejercicio useState con un hook puesto mas bonito

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -3,16 +3,12 @@ import {useCounter} from '../hooks/useCounter'
 
 export const CounterWithCustomHook = () => {
 
-
 const {counter, increment , decrement , reset} = useCounter();
-
-
 
   return (
     <div className='container-fluid'>
         <h1>Counter With Hook : {counter}</h1>
         <hr />
-
         <button className="btn btn-primary" onClick={()=>increment(2)}>+1</button>
         <button className="btn btn-primary"onClick={reset}>Reset</button>
         <button className="btn btn-primary"onClick={()=>decrement()}>-1</button>
